f0915e14f1f1fcf794bead2dce326da390670ff0
bruno.barp <bruno.barp@stw.com.br>
2026-06-21 10:43:08 -0300

    test: adiciona quebra de formatacao psr12 e sintaxe

--- cadastro_receita.php
+++ cadastro_receita.php
@@ -12,7 +12,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     require_once 'conexao.php';
 
     $nome = trim($_POST['nome']);
-    $x = 1;
+    ECHO "testando erro";
     $descricao = trim($_POST['descricao']);
     $tipo_receita = $_POST['tipo_receita'];
 
