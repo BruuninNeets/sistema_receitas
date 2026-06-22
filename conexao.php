1f3a99a77c4606495e10c714357d0b5617c933be
bruno.barp <bruno.barp@stw.com.br>
2026-06-22 20:48:47 -0300

    testando

--- conexao.php
+++ conexao.php
@@ -6,6 +6,7 @@ $senha = getenv('DB_PASS');
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $senha);
+    $pdo->exec("CREATE TABLE IF NOT EXISTS categoria (id INT AUTO_INCREMENT PRIMARY KEY, descricao VARCHAR(255) NOT NULL);");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro ao conectar com BD: " . $e->getMessage());
